d15ce76b977dbca3a204661d7b7a75a9c1b85edd
Katie Holly <git@meo.ws>
2020-02-08 13:26:25 +0100

    fix bug causing rsyncd to not get killed when reaching the hard limit

--- files/controller.sh
+++ files/controller.sh
@@ -13,9 +13,14 @@ rsync_stopped=1
 
 supervisorctl stop rsyncd
 
+find /data/incoming/ -type f '(' -wholename "*/.rsync-tmp/*" -o -name "*.warc.gz.*" ')' -delete
+find /data/incoming/ -mindepth 1 -type d -empty -delete
+
 while true; do
 	disk_usage=$(df --output=pcent /data/ | tr -dc '0-9')
+	echo "disk_usage=${disk_usage}/${DISK_LIMIT:-60}/${DISK_HARD_LIMIT:-85}"
 	if test -z "${disk_usage}"; then
+		echo "killing rsyncd"
 		supervisorctl stop rsyncd && rsync_stopped=1
 		pkill -9 -f '^/usr/bin/rsync '
 		echo "/data is not a volume"
@@ -23,7 +28,8 @@ while true; do
 		continue
 	fi
 	if test "${disk_usage}" -gt "${DISK_HARD_LIMIT:-85}"; then
-		if test -n "${rsync_stopped}"; then
+		if test -z "${rsync_stopped}"; then
+			echo "killing rsyncd"
 			supervisorctl stop rsyncd && rsync_stopped=1
 			pkill -9 -f '^/usr/bin/rsync '
 			find /data/incoming/ -type f '(' -wholename "*/.rsync-tmp/*" -o -name "*.warc.gz.*" ')' -delete
@@ -31,7 +37,7 @@ while true; do
 		fi
 		sleep 5
 		continue
-	elif test "${disk_usage}" -gt "${DISK_LIMIT:-75}"; then
+	elif test "${disk_usage}" -gt "${DISK_LIMIT:-60}"; then
 		tgt_conn=-1
 	else
 		tgt_conn="${MAX_CONN:-100}"
@@ -42,5 +48,9 @@ while true; do
 		find /data/incoming/ -mindepth 1 -type d -empty -delete
 		supervisorctl start rsyncd && rsync_stopped=
 	fi
-	sleep 5
+	if test "${tgt_conn}" == "-1" && test -z "${rsync_stopped}"; then
+		sleep 0.5
+	else
+		sleep 1
+	fi
 done
