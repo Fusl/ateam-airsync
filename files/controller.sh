4950bf058421b1c64c6aeb48c64ca48a7559d542
Katie Holly <git@meo.ws>
2020-09-29 22:48:05 +0200

    add port option to support host network

--- files/controller.sh
+++ files/controller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 test -d /data || {
-	echo "No /data mount found"
+	echo 'No /data mount found'
 	exit 1
 }
 
@@ -14,7 +14,7 @@ rsync_stopped=1
 supervisorctl stop rsyncd
 
 cleanup_temp() {
-	find /data/incoming/ -type f '(' -wholename "*/.rsync-tmp/*" -o -name "*.warc.gz.*" -o -name "*.warc.zst.*" ')' -delete
+	find /data/incoming/ -type f '(' -wholename '*/.rsync-tmp/*' -o -name '*.warc.gz.*' -o -name '*.warc.zst.*' ')' -delete
 	find /data/incoming/ -mindepth 1 -type d -empty -delete
 }
 
@@ -24,16 +24,16 @@ while true; do
 	disk_usage=$(df --output=pcent /data/ | tr -dc '0-9')
 	echo "disk_usage=${disk_usage}/${DISK_LIMIT:-60}/${DISK_HARD_LIMIT:-85}"
 	if test -z "${disk_usage}"; then
-		echo "killing rsyncd"
+		echo 'killing rsyncd'
 		supervisorctl stop rsyncd && rsync_stopped=1
 		pkill -9 -f '^/usr/bin/rsync '
-		echo "/data is not a volume"
+		echo '/data is not a volume'
 		sleep 5
 		continue
 	fi
 	if test "${disk_usage}" -gt "${DISK_HARD_LIMIT:-85}"; then
 		if test -z "${rsync_stopped}"; then
-			echo "killing rsyncd"
+			echo 'killing rsyncd'
 			supervisorctl stop rsyncd && rsync_stopped=1
 			pkill -9 -f '^/usr/bin/rsync '
 			cleanup_temp
@@ -50,7 +50,7 @@ while true; do
 		cleanup_temp
 		supervisorctl start rsyncd && rsync_stopped=
 	fi
-	if test "${tgt_conn}" == "-1" && test -z "${rsync_stopped}"; then
+	if test "${tgt_conn}" == '-1' && test -z "${rsync_stopped}"; then
 		sleep 0.5
 	else
 		sleep 1
